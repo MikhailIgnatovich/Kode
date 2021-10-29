41cd70be4108b4e71d7f83a40b6bc4bd4babeed1
MikhailIgnatovich <bulich198922@gmail.com>
2021-10-29 17:29:35 +0300

    Поделил проект на слои data, domain, presentation

--- app/src/main/java/com/bulich/misha/kode/presentation/MainActivity.kt
+++ app/src/main/java/com/bulich/misha/kode/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bulich.misha.kode
+package com.bulich.misha.kode.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bulich.misha.kode.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
