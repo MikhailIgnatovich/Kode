964464708a2251ca39d2c917114543076e8cdda3
MikhailIgnatovich <bulich198922@gmail.com>
2021-10-29 18:24:25 +0300

    Добавил разметку top app bar с SearchView в fragment_home.xml

--- app/src/main/java/com/bulich/misha/kode/presentation/MainActivity.kt
+++ app/src/main/java/com/bulich/misha/kode/presentation/MainActivity.kt
@@ -3,10 +3,13 @@ package com.bulich.misha.kode.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bulich.misha.kode.R
+import com.bulich.misha.kode.presentation.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- app/src/main/java/com/bulich/misha/kode/presentation/fragments/HomeFragment.kt
+++ app/src/main/java/com/bulich/misha/kode/presentation/fragments/HomeFragment.kt
@@ -6,15 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bulich.misha.kode.R
+import com.bulich.misha.kode.databinding.FragmentHomeBinding
+import java.lang.RuntimeException
 
 
 class HomeFragment : Fragment() {
-
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentHomeBinding == null")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
