fcbb6ae2830612238ffe07cb5eb730a40dbe263b
MikhailIgnatovich <bulich198922@gmail.com>
2021-10-30 22:11:18 +0300

    Настроил retrofit в ApiModule.kt через dagger

--- app/src/main/java/com/bulich/misha/kode/presentation/di/ApiModule.kt
+++ app/src/main/java/com/bulich/misha/kode/presentation/di/ApiModule.kt
@@ -1,7 +1,41 @@
 package com.bulich.misha.kode.presentation.di
 
+import com.bulich.misha.kode.data.ApiService
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
+@Singleton
 @Module
 class ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideGetOkHttpClient():OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGetRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://stoplight.io/mocks/kode-education/trainee-test/25143926")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGetApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
 }
